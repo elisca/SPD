d04135c07087986d50122720e0b902d6859270c6
Ezequiel Lisca <ezequiel.lisca@live.com>
2017-11-09 20:23:54 -0300

    LCD con Keypad Shield - Reconocer boton presionado
    Muestra en display que boton se esta presionando en el keypad shield, en caso de estar presionando alguno.

---
+++ LCD_Keypad_Shield_Reconocer_Boton_Presionado/LCD_Keypad_Shield_Reconocer_Boton_Presionado.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,9,4,5,6,7);
+int entrada;
+
+void setup() {
+  lcd.begin(16,2);
+}
+
+void loop() {
+  entrada=analogRead(A0);
+
+  if(entrada>882&&entrada<=1023)
+    lcd.print("En reposo");
+  if(entrada>623&&entrada<=882)
+    lcd.print("Select");
+  if(entrada>431&&entrada<=623)
+    lcd.print("Izquierda");
+  if(entrada>237&&entrada<=431)
+    lcd.print("Abajo");
+  if(entrada>72&&entrada<=237)
+    lcd.print("Arriba");
+  if(entrada>=0&&entrada<=72)
+    lcd.print("Derecha");
+  
+  delay(50);
+  lcd.clear();
+}
