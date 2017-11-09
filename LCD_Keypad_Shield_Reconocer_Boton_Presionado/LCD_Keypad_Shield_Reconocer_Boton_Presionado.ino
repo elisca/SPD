216f3629b2048507291cdda6a496088ca0cf353c
Ezequiel Lisca <ezequiel.lisca@live.com>
2017-11-09 20:42:37 -0300

    LCD Keypad Shield - Reconoce boton presionado
    Codigo optimizado terminado y funcional.

--- LCD_Keypad_Shield_Reconocer_Boton_Presionado/LCD_Keypad_Shield_Reconocer_Boton_Presionado.ino
+++ LCD_Keypad_Shield_Reconocer_Boton_Presionado/LCD_Keypad_Shield_Reconocer_Boton_Presionado.ino
@@ -1,28 +1,51 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8,9,4,5,6,7);
-int entrada;
+int entrada,cod_puls;
 
 void setup() {
   lcd.begin(16,2);
 }
 
 void loop() {
-  entrada=analogRead(A0);
-
-  if(entrada>882&&entrada<=1023)
-    lcd.print("En reposo");
-  if(entrada>623&&entrada<=882)
-    lcd.print("Select");
-  if(entrada>431&&entrada<=623)
-    lcd.print("Izquierda");
-  if(entrada>237&&entrada<=431)
-    lcd.print("Abajo");
-  if(entrada>72&&entrada<=237)
-    lcd.print("Arriba");
-  if(entrada>=0&&entrada<=72)
-    lcd.print("Derecha");
+  switch(keypad_shield()){
+    case -1:
+      lcd.print("En reposo");
+      break;
+    case 0:
+      lcd.print("Derecha");
+      break;
+    case 1:
+      lcd.print("Arriba");
+      break;
+    case 2:
+      lcd.print("Abajo");
+      break;
+    case 3:
+      lcd.print("Izquierda");
+      break;
+    case 4:
+      lcd.print("Select");
+      break;
+  }
   
   delay(50);
   lcd.clear();
+}
+
+int keypad_shield(){
+  entrada=analogRead(A0);
+  
+  if(entrada>623&&entrada<=882)//Select
+    return 4;
+  if(entrada>431&&entrada<=623)//Izquierda
+    return 3;
+  if(entrada>237&&entrada<=431)//Abajo
+    return 2;
+  if(entrada>72&&entrada<=237)//Arriba
+    return 1;
+  if(entrada>=0&&entrada<=72)//Derecha
+    return 0;
+  
+  return -1;//En reposo
 }
